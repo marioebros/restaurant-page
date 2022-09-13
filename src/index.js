b481925e9869173a5e187ed16325f7ae6bab104c
Mario Elizaldegit config --global user.email dollarize.mme@gmail.com <dollarize.mme@gmail.com>
2022-09-13 16:54:27 -0700

    following along webpack guide

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
+import _ from "lodash";
+
 console.log("Issa my restaurant page, yo!");
 
-function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+function component() {
+  const element = document.createElement("div");
 
-  // Remove the background color of all tablinks/buttons
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].style.backgroundColor = "";
-  }
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  // Show the specific tab content
-  document.getElementById(pageName).style.display = "block";
-
-  // Add the specific color to the button used to open the tab content
-  elmnt.style.backgroundColor = color;
+  return element;
 }
 
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+document.body.appendChild(component());
