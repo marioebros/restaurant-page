d885f033cfd3382890d7966699a786eaef34a295
Mario Elizaldegit config --global user.email dollarize.mme@gmail.com <dollarize.mme@gmail.com>
2022-09-15 17:33:29 -0700

    updated with correct function calls

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
-import * as module from "./landing-page-load.js";
+import { landingPageLoad } from "./landing-page-load.js";
+import { menu } from "./menu.js";
+import { contact } from "./contact.js";
 
-console.log("This is my console.log statement from /src/index.js");
-module.landingPageLoad();
+landingPageLoad();
+
+// Tab Switching Module
+let tabSwitchingModule = (function () {
+  const homeTab = document.querySelector(".home");
+  homeTab.addEventListener("click", landingPageLoad);
+
+  const menuTab = document.querySelector(".menu");
+  menuTab.addEventListener("click", menu);
+
+  const contactTab = document.querySelector(".contact");
+  contactTab.addEventListener("click", contact);
+})();
